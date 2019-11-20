c7dcb26313894f222e6cf7d8ed27836d5b2535f5
omer4 <omer404455@gmail.com>
2019-11-20 15:14:47 +0200

    +ability to add things to firebase

--- Alphatest/app/src/main/java/com/example/alpha_test/UploadPictures.java
+++ Alphatest/app/src/main/java/com/example/alpha_test/UploadPictures.java
@@ -1,69 +1,136 @@
 package com.example.alpha_test;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 public class UploadPictures extends AppCompatActivity {
+    ImageView img;
+    Button ch;
+
+
+    StorageReference mStorageRef;
+    private StorageTask uploadTask;
+
+    public Uri imguri;
+
+    public static final int IMAGE_PICK_CODE=1000;
+    public static final int PERMISSION_CODE=1001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_pictures);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images");
+
+        img = findViewById(R.id.iv);
+        ch = findViewById(R.id.Chs);
+
+
+    }
+
+    public void FileChooser(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                requestPermissions(permissions,PERMISSION_CODE);
+            } else {
+                pickFromGallery();
+            }
+
+
+        } else {
+            pickFromGallery();
+
+        }
+
     }
 
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    pickFromGallery();
+                }
+                else{
+                    Toast.makeText(this,"Permission denied",Toast.LENGTH_SHORT).show();
+                }
+            }
+    }
+    }
+    private void pickFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_PICK_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imguri = data.getData();
+            img.setImageURI(imguri);
+            Toast.makeText(this,"We are uploading your file...",Toast.LENGTH_SHORT).show();
+            upload();
+        }
+    }
+
+    private String getExtension(Uri uri){
+        ContentResolver cr=getContentResolver();
+        MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+    }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void upload() {
+        StorageReference Ref=mStorageRef.child(System.currentTimeMillis()+"."+getExtension(imguri));
+        uploadTask=Ref.putFile(imguri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Toast.makeText(getApplicationContext(),"Image uploaded successfully",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+    }
 
 
 
@@ -77,29 +144,30 @@ public class UploadPictures extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
-    public void AuthScreen (MenuItem item){
+    public void AuthScreen(MenuItem item) {
         Intent t = new Intent(this, MainActivity.class);
         startActivity(t);
     }
 
-    public void RemoveScreen (MenuItem item){
+    public void RemoveScreen(MenuItem item) {
         Intent t = new Intent(this, AddData.class);
         startActivity(t);
 
     }
 
-    public void ImageScreen (MenuItem item){
+    public void ImageScreen(MenuItem item) {
         Intent t = new Intent(this, UploadPictures.class);
         startActivity(t);
 
     }
 
-    public void ScanScreen (MenuItem item){
+    public void ScanScreen(MenuItem item) {
         Intent t = new Intent(this, BarcodeScan.class);
         startActivity(t);
     }
 }
+
